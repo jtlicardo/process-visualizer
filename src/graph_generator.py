1897d47fe95ceef97ba2bbab21b3b2ade43c6e4c
Josip Tomo Licardo <licardo.josip@gmail.com>
2023-03-27 17:53:37 +0200

    Update graph_generator.py

--- src/graph_generator.py
+++ src/graph_generator.py
@@ -459,7 +459,12 @@ class GraphGenerator:
                     f"{previous_element['id']}",
                     f"{element['id']}_S",
                 )
-        
+            else:
+                self.connect(
+                    f"{previous_element['id']}_E",
+                    f"{element['id']}_S",
+                )
+
         if previous_element is None and parent_gateway is not None:
             self.connect(
                 f"{parent_gateway['id']}_S",
